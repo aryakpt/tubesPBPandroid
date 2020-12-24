a4fb9f889d1311c16fb417f6cbeb73c8a886fa50
aryakpt <aryakrisna07@gmail.com>
2020-12-24 14:52:49 +0700

    first commit

---
+++ lib/detail.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Detail extends StatefulWidget {
+  List list;
+  int index;
+  Detail({this.index, this.list});
+  @override
+  _DetailState createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+            backgroundColor: Colors.yellow[800],
+            title: new Text("${widget.list[widget.index]['judul']}")),
+        body: new Container(
+            padding: const EdgeInsets.all(10),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                    child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                      new Padding(padding: const EdgeInsets.only(top: 5)),
+                      new Padding(padding: const EdgeInsets.only(bottom: 10)),
+                      new Text("${widget.list[widget.index]['judul']}",
+                          style: new TextStyle(fontSize: 18)),
+                      new Text(
+                          "Penulis : ${widget.list[widget.index]['penulis']}",
+                          style: new TextStyle(fontSize: 14)),
+                      new Text(
+                          "Dipost pada: ${widget.list[widget.index]['tgl']}",
+                          style: new TextStyle(fontSize: 14)),
+                    ])),
+                new Padding(padding: const EdgeInsets.only(top: 10)),
+                new Container(
+                    child: new Flexible(
+                  child: new SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child: new Text(widget.list[widget.index]['isi'],
+                        style: new TextStyle(fontSize: 12)),
+                  ),
+                )),
+                new Card(
+                    child: new Container(
+                  child: ListTile(
+                      subtitle:
+                          new Text(widget.list[widget.index]['komentar'])),
+                ))
+              ],
+            )));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import './detail.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Aurora",
+    home: new Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<List> getData() async {
+    final response = await http
+        .get("http://10.0.2.2//WebDinamis/webservices/get_recent_post.php");
+    return json.decode(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.yellow[800],
+        leading: new Icon(Icons.home),
+        title: new Center(child: Text("Aurora")),
+        actions: <Widget>[new Icon(Icons.search)],
+      ),
+      body: new FutureBuilder<List>(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+          return snapshot.hasData
+              ? new ItemList(list: snapshot.data)
+              : new Center(
+                  child: new CircularProgressIndicator(),
+                );
+        },
+      ),
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+  final List list;
+  ItemList({this.list});
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      itemCount: list == null ? 0 : list.length,
+      itemBuilder: (context, i) {
+        return Container(
+          padding: const EdgeInsets.all(1),
+          child: GestureDetector(
+            onTap: () => Navigator.of(context).push(new MaterialPageRoute(
+                builder: (BuildContext context) => new Detail(
+                      list: list,
+                      index: i,
+                    ))),
+            child: new Card(
+              child: Container(
+                padding: const EdgeInsets.all(5),
+                child: new ListTile(
+                  title: new Text(list[i]['judul']),
+                  leading: new Icon(Icons.book),
+                  subtitle: new Text(
+                      "Penulis : ${list[i]['penulis']}\nDipost pada : ${list[i]['tgl']}"),
+                  //subtitle: new Text("Dipost pada : ${list[i]['tgl_insert']}"),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
