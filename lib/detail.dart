df08cf38ece8750d446e80fa3e42e0203c74ed64
aryakpt <aryakrisna07@gmail.com>
2020-12-25 21:56:47 +0700

    commit detail.dart ulang

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class Detail extends StatefulWidget {
   List list;
@@ -9,6 +12,27 @@ class Detail extends StatefulWidget {
 }
 
 class _DetailState extends State<Detail> {
+  Future<List> getKomen() async {
+    final response =
+        await http.get("http://10.0.2.2//WebDinamis/webservices/get_komen.php");
+    return json.decode(response.body);
+  }
+
+  Widget widgetFutureBuilder() {
+    return FutureBuilder<List>(
+      future: getKomen(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) print(snapshot.error);
+        return snapshot.hasData
+            ? new DetailKomen(
+                list: snapshot.data, id: widget.list[widget.index]['id'])
+            : new Center(
+                child: new CircularProgressIndicator(),
+              );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -21,8 +45,7 @@ class _DetailState extends State<Detail> {
             child: new ListView(
               padding: const EdgeInsets.all(10),
               children: <Widget>[
-                //Image.network(
-                // "/data:image/jpeg;base64${widget.list[widget.index]['gambar']}"),
+                Image.memory(base64Decode(widget.list[widget.index]['gambar'])),
                 new Container(
                     child: new Column(children: <Widget>[
                   new Padding(padding: const EdgeInsets.only(top: 5)),
@@ -40,15 +63,39 @@ class _DetailState extends State<Detail> {
                   child: new Text(widget.list[widget.index]['isi'],
                       style: new TextStyle(fontSize: 12)),
                 ),
-                // new Card(
-                //     child: new Container(
-                //   child: ListTile(
-                //       subtitle:
-                //           new Text(widget.list[widget.index]['komentar'])),
-                // ))
+                widgetFutureBuilder(),
               ],
             ),
           ),
         )));
+  }
+}
+
+class DetailKomen extends StatelessWidget {
+  final List list;
+  final id;
+  DetailKomen({this.list, this.id});
+  @override
+  Widget build(BuildContext context) {
+    return new ListView.builder(
+      itemCount: list == null ? 0 : list.length,
+      itemBuilder: (context, i) {
+        if (list[i]['id'] == id) {
+          return Container(
+            padding: const EdgeInsets.all(1),
+            child: new Card(
+              child: Container(
+                padding: const EdgeInsets.all(5),
+                child: new ListTile(
+                  title: new Text(list[i]['komentar']),
+                ),
+              ),
+            ),
+          );
+        } else {
+          return Container();
+        }
+      },
+    );
   }
 }
